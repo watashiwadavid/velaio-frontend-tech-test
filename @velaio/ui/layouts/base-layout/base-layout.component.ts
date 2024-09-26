50793ca68355f5a1adf26bf4e270f01598eee3ad
PC-DAVID <david.lopez.enggmail.com>
2024-09-26 12:53:08 -0500

    feat(layout): add base files for page layout
    - Added initial files and structure for the application layout.
    - Includes base layout components and related setup.

---
+++ @velaio/ui/layouts/base-layout/base-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'velaio-base-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './base-layout.component.html',
+  styleUrls: ['./base-layout.component.scss'],
+})
+export class BaseLayoutComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('@velaio/ui/layouts').then((ly) => ly.BaseLayoutComponent),
+  },
+];
