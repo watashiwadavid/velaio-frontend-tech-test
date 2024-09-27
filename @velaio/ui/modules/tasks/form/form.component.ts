28bf3cc0c37a8a0dbc655677b46a36e32ed6dc0f
PC-DAVID <david.lopez.enggmail.com>
2024-09-26 21:15:31 -0500

    feat(tasks): add base files for task listing and creation forms
    - Create base files for task listing module
    - Add task creation form template

---
+++ @velaio/ui/modules/tasks/form/form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'velaio-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent {
+
+}
---
+++ @velaio/ui/modules/tasks/list/list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'velaio-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent {
+
+}
