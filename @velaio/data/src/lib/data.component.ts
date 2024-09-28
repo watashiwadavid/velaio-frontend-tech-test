2f0325dfc4a13e8f5bffff5604eb2d4b0d89d820
PC-DAVID <david.lopez.enggmail.com>
2024-09-28 16:57:24 -0500

    core(data):  Add base files for data library

---
+++ @velaio/data/src/lib/data.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'velaio-data',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      data works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DataComponent {
+
+}
---
+++ @velaio/data/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of data
+ */
+
+export * from './lib/data.service';
+export * from './lib/data.component';
